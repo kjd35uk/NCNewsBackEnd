8cd882107d78abed72974862e19cbfd198dec99b
Kirstie Davidson <kjd35uk@yahoo.co.uk>
2018-06-18 14:05:35 +0100

    Adding get articles by user controller/route

--- controllers/users.js
+++ controllers/users.js
@@ -1,20 +1,39 @@
-const { User } = require("../models");
+const { User, Article } = require("../models");
+const { addCommentCount } = require("../utils");
 
 exports.getUsers = (req, res, next) => {
   User.find()
     .then(users => {
-      if(users !== null) res.send({ users });
+      if (users !== null) res.send({ users });
     })
     .catch(next);
 };
-
 
 exports.getUserByUsername = (req, res, next) => {
   //console.log("getting user by username");
   User.findOne({ username: req.params.username })
     .then(user => {
-     res.send({ user });
+      res.send({ user });
     })
-    .catch(err => 
-      next({status: 404, msg: `${req.params.username} not found`}))
-  };
+    .catch(err =>
+      next({ status: 404, msg: `${req.params.username} not found` })
+    );
+};
+
+exports.getArticlesByUserId = (req, res, next) => {
+  console.log("getting articles by user id");
+  console.log(req.params.username, "REQ");
+  Article.find({ created_by: req.params.username })
+    .populate("created_by")
+    .lean()
+    .then(articles => {
+      console.log(articles, "ARTICLES");
+      return Promise.all([...articles.map(addCommentCount)]);
+    })
+    .then(articles => {
+      articles.length === 0
+        ? next({ status: 404, msg: `${req.params._id} not found` })
+        : res.send({ articles });
+    })
+    .catch(next);
+};
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const { getUsers, getUserByUsername } = require("../controllers/users");
+const { getUsers, getUserByUsername, getArticlesByUserId } = require("../controllers/users");
 
 router.get("/", getUsers);
 router.get("/:username", getUserByUsername);
+router.get("/:username/articles", getArticlesByUserId);
 
 router.use('/*', (req, res, next) => {
   next({status: 404, msg: 'Page not found'})
